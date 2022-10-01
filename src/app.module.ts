ed49347d474f3ff3c96e374be314728178a2c5d9
Galin Kostov <dev@galin.cc>
2022-10-01 17:50:55 +0200

    Initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'chewbacca',
+      password: 'rawr',
+      database: 'kashyyyk',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
---
+++ src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserResolver } from './user.resolver';
+
+@Module({
+  imports: [],
+  providers: [UserResolver],
+  exports: [],
+})
+export class UserModule {
+}
